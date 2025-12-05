c77d72f0a231b61764c0366303c61cc2a5006718
Kris Hedges <kris.hedges@movehq.com>
2025-12-04 23:39:42 -0500

    feat(deployment): first pass at cf workers hosting
     Changes to be committed:
    	new file:   .open-next/.build/open-next.config.edge.mjs
    	new file:   .open-next/.build/open-next.config.mjs
    	modified:   next-env.d.ts
    	new file:   open-next.config.ts
    	modified:   package.json
    	modified:   proxy.ts
    	new file:   wrangler.jsonc
    	modified:   yarn.lock

--- proxy.ts
+++ proxy.ts
@@ -78,6 +78,6 @@ export const config = {
      * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
      * Feel free to modify this pattern to include more paths.
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\.(?:svg|png|jpg|jpeg|gif|webp|wav)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|.*.(?:svg|png|jpg|jpeg|gif|webp|wav)$).*)',
   ],
 }
